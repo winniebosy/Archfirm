9ad4035bd3e8b4e7023866ebd4cda5ae90bd66e6
Winnie Bosibori <winniebosy@gmail.com>
2022-07-02 13:48:12 +0300

    modifications of the project page

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 let menubtn = document.getElementById('toggle')
 let menuNav = document.querySelector('.nav-menu');
 
+
 //opens and closes menu
-function openMenu() {
+menubtn.addEventListener('click', function(e) {
     menuNav.classList.toggle("hidden");
     menubtn.setAttribute('aria-expanded', true);
-}
 
-//event listener
-menubtn.addEventListener('click', openMenu);
+    //closes menu when user clicks outside the menu.
+    document.addEventListener('click', function(e) {
+        let inside = (e.target.closest('div'));
+        if (!inside) {
+            menuNav.classList.add('hidden');
+        }
+    });
+});
+
+//closes the menu when user presses the escape key
+window.addEventListener('keydown', function(e) {
+    if (e.key === 27 || e.key === 'Escape') {
+        menuNav.classList.add('hidden');
+    }
+})
 
 
 //FOCUS TRAPPING KEYBOARD
@@ -16,7 +29,7 @@ function trapFocus() {
     /* FOCUS TRAP METHOD  */
     //select all the elements that need to be focused
     let focusElements = document.querySelectorAll('button[id*="toggle"], a[class*="nav-list-link"');
-    console.log(focusElements);
+    //console.log(focusElements);
     //select the first element to be focused
     let firstFocusElement = focusElements[0];
     //last element to be focused
@@ -26,21 +39,20 @@ function trapFocus() {
     focusElements.forEach(focusElement => {
         //listen to the keydown event in each node
         focusElement.addEventListener('keydown', function(e) {
-            let code = e.key;
-            if (!code) {
-                return; /* breaks the execution */
-            }
+
             if (e.shiftKey) /* shift + tab */ {
+                // focuses on last element
                 if (document.activeElement === firstFocusElement) {
                     lastFocusElement.focus();
                     e.preventDefault();
-                    console.log('focused on first last element');
+
                 }
-            } else { /* if tab ke is selected */
+            } else { /* if tab key is selected */
                 if (document.activeElement === lastFocusElement) {
+                    // focuses on last element
                     firstFocusElement.focus();
                     e.preventDefault();
-                    console.log('focused on first element');
+
 
                 }
 
@@ -52,50 +64,3 @@ function trapFocus() {
 }
 
 trapFocus();
-
-
-
-
-
-
-
-
-
-// query all elements that need to be focused
-
-
-// li[class*="nav-list-item"');
-
-
-// select the first element to be focused;
-//let firstFocusElement = focusElements[0];
-//last element to be focused
-//let lastFocusElement = focusElements[focusElements - 1];
-//listen to keydown event
-
-//let KEYCODE_TAB = 9;
-
-/* document.addEventListener('keydown', function(e) {
-
-   let isTabPressed = (e.key === 'Tab' || e.key === KEYCODE_TAB);
-
-   if (!isTabPressed) {
-       return;
-   }
-   if (e.shiftKey) //shift + tab 
-{
-    if (document.activeElement === firstFocusElement) {
-        lastFocusElement.focus();
-        e.preventDefault();
-    } else // using tab {
-        if (document.activeElement === lastFocusElement) {
-            firstFocusElement.focus();
-            e, preventDefault();
-
-        }
-    }
-
-
-}
-
-}); */
