23470c919794ab4604cd60a5e844dcfaaf687df3
Winnie Bosibori <winniebosy@gmail.com>
2022-08-01 17:52:15 +0300

    modifications for a11y purposes

--- index.js
+++ index.js
@@ -17,50 +17,54 @@ menubtn.addEventListener('click', function(e) {
 });
 
 //closes the menu when user presses the escape key
-window.addEventListener('keydown', function(e) {
+menubtn.addEventListener('keydown', function(e) {
     if (e.key === 27 || e.key === 'Escape') {
         menuNav.classList.add('hidden');
     }
 })
 
+function detectscreen() {
+    if (screen.width <= 600) {
+        //FOCUS TRAPPING KEYBOARD
+        function trapFocus() {
+            /* FOCUS TRAP METHOD  */
+            //select all the elements that need to be focused
+            let focusElements = document.querySelectorAll('button[id*="toggle"], a[class*="nav-list-link"');
+            //console.log(focusElements);
+            //select the first element to be focused
+            let firstFocusElement = focusElements[0];
+            //last element to be focused
+            let lastFocusElement = focusElements[focusElements.length - 1];
 
-//FOCUS TRAPPING KEYBOARD
-function trapFocus() {
-    /* FOCUS TRAP METHOD  */
-    //select all the elements that need to be focused
-    let focusElements = document.querySelectorAll('button[id*="toggle"], a[class*="nav-list-link"');
-    //console.log(focusElements);
-    //select the first element to be focused
-    let firstFocusElement = focusElements[0];
-    //last element to be focused
-    let lastFocusElement = focusElements[focusElements.length - 1];
+            //loop through the node list
+            focusElements.forEach(focusElement => {
+                //listen to the keydown event in each node
+                focusElement.addEventListener('keydown', function(e) {
 
-    //loop through the node list
-    focusElements.forEach(focusElement => {
-        //listen to the keydown event in each node
-        focusElement.addEventListener('keydown', function(e) {
+                    if (e.shiftKey) /* shift + tab */ {
+                        // focuses on last element
+                        if (document.activeElement === firstFocusElement) {
+                            lastFocusElement.focus();
+                            e.preventDefault();
 
-            if (e.shiftKey) /* shift + tab */ {
-                // focuses on last element
-                if (document.activeElement === firstFocusElement) {
-                    lastFocusElement.focus();
-                    e.preventDefault();
-
-                }
-            } else { /* if tab key is selected */
-                if (document.activeElement === lastFocusElement) {
-                    // focuses on last element
-                    firstFocusElement.focus();
-                    e.preventDefault();
+                        }
+                    } else { /* if tab key is selected */
+                        if (document.activeElement === lastFocusElement) {
+                            // focuses on last element
+                            firstFocusElement.focus();
+                            e.preventDefault();
 
 
-                }
+                        }
 
-            }
+                    }
 
-        });
-    });
+                });
+            });
 
+        }
+
+        trapFocus();
+    }
 }
-
-trapFocus();
+detectscreen()
