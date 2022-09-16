bc98e0ae52043bc5ab58133ee641c609f9d513cb
Winnie Bosibori <winniebosy@gmail.com>
2022-09-16 13:51:01 +0300

    email and phonenumber validation

--- signup.js
+++ signup.js
@@ -1,10 +1,11 @@
 inputFirstName = document.querySelector('#firstName');
-inputLastName = document.querySelector('#lastName')
+inputLastName = document.querySelector('#lastName');
+let phoneNumber = document.querySelector('#telNumber')
     // console.log(inputFirstName)
     //listening to user input for first name
 inputFirstName.addEventListener('input', (e) => {
     let userInput = inputFirstName.value;
-    console.log(userInput)
+    //console.log(userInput)
     let errDisplay = document.querySelector('#fnameError')
     if (userInput === '') {
         errDisplay.textContent = "Error: first name field cannot be empty";
@@ -33,3 +34,46 @@ inputLastName.addEventListener('input', (e) => {
     }
 
 });
+
+
+//validating telephone number
+function validatePhoneNumber(telnumber) {
+    const re = /^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/;
+    return re.test(telnumber)
+
+}
+
+phoneNumber.addEventListener('input', (e) => {
+    let userTelNumber = phoneNumber.value;
+    let phoneErrorDisplay = document.querySelector('#telError');
+    if (!validatePhoneNumber(userTelNumber)) {
+        phoneErrorDisplay.textContent = 'Error: wrong telephone number format';
+        phoneErrorDisplay.focus()
+    } else if (validatePhoneNumber(userTelNumber)) {
+        phoneErrorDisplay.textContent = '✔️ OK';
+    } else {
+        phoneErrorDisplay.textContent = " ";
+    }
+})
+
+
+//validating email
+function validateEmail(mail) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(String(mail).toLowerCase());
+
+}
+
+let email = document.querySelector('#email');
+
+email.addEventListener('input', (e) => {
+    let userEmail = email.value;
+    let mailError = document.querySelector('#emailError');
+
+    if (validateEmail(userEmail)) {
+        mailError.textContent = '✔️ OK';
+    } else if (!validateEmail(userEmail)) {
+        mailError.textContent = 'Error: wrong email format';
+        mailError.focus()
+    }
+})
